d298820611b046e24fc7077badfdf4108b7ef164
The-Mathemagician <42478882+The-Mathemagician@users.noreply.github.com>
2018-09-24 11:19:05 -0500

    Add files via upload

--- Quadratic/CompleteTheSquare.java
+++ Quadratic/CompleteTheSquare.java
@@ -1,129 +1,153 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 /**
- * Write a description of class CompleteTheSquare here.
+ * This program will complete the square on a quadratic expression with
+ * constants given by the user.  Each step will be shown clearly so that
+ * an Algebra 2 student can check their handwritten work.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Brandon Bettes
+ * @version 1.0
  */
 public class CompleteTheSquare
 {
     public static void main(String[] args)
     {
         Scanner kb = new Scanner(System.in);
-        double a = 0, b = 0, c = 0;
+
         String answer = "";
-        
+        double a = 0, b = 0, c = 0;
+
         do
         {
             System.out.println("Please enter the constants A, B, and C for the following");
-            System.out.println("quadratic expression Ax^2 + Bx + C: ");
-            
+            System.out.println("quadratic equation A*x^2 + B*x + C:");
+
+            // get the value of A from the user
             System.out.print("A = ");
             a = kb.nextDouble();
-            
+
+            // get the value of B from the user
             System.out.print("B = ");
             b = kb.nextDouble();
-            
+
+            // get the value of C from the user
             System.out.print("C = ");
             c = kb.nextDouble();
-            
-            System.out.println();
-            
-            System.out.println( a + "x^2 + " + b + "x + " + c );
-            
-            System.out.print("Is this the correct quadratic? Y or N ");
-            
-            
-            
+
+            // Display the current quadratic
+            System.out.println( a + "*x^2 + " + b + "*x + " + c );
+
+            // Prompt user if this is correct
+            System.out.print("Is this the correct quadratic? (Y or N) ");
             answer = kb.next();
-            
-            System.out.println();
-            
-        }while( !answer.equalsIgnoreCase("Y") );
-        
+
+        } while( !answer.equalsIgnoreCase("Y") );
+
         // Step 0
         System.out.println();
         System.out.println();
         System.out.println("Step 0: Group the terms that have x^2 and x.");
         System.out.println();
-        System.out.println( "(" + a + "x^2 + " + b + "x) + " + c );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        System.out.println( "(" + a + "*x^2 + " + b + "*x) + " + c );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
-        
-        
+
         // Step 1
-        System.out.println();{ }
+        System.out.println();
         System.out.println();
         System.out.println("Step 1: Factor out the number being multiplied by");
-        System.out.println("the x^2 term from the x^2 and x terms.");
+        System.out.println("        the x^2 term from the x^2 and x terms.");
         System.out.println();
-        System.out.println( a + "(x^2 + " + (b/a) + "x) + " + c );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        System.out.println( a + "(x^2 + " + (b/a) + "*x) + " + c );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
-        
-        
+
         // Step 2
         System.out.println();
         System.out.println();
-        System.out.println("Step 2: Compute side work: Take the number being");
-        System.out.println("multiplied by the x term, divide by 2 and square the");
-        System.out.println("result.");
+        System.out.println("Step 2: Compute side work: Take the number being multiplied");
+        System.out.println("        by the x term, divide by 2 and square the result.");
         System.out.println();
-        double num = (b*b/(4*a*a));
-        System.out.println( "((" + (b/a) + ")/2)^2 = (" + (b/(2*a)) + ")^2 = " + num );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        double num = Math.pow( (b/a)/2, 2 );
+        System.out.println( " ((" + (b/a) + ")/2)^2 = " + num );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
-        
-        
+
         // Step 3
         System.out.println();
         System.out.println();
         System.out.println("Step 3: Add and subtract a 'Well Chosen Zero', that is,");
-        System.out.println("add and subtract the number computed in Step 2 inside");
-        System.out.println("the parentheses.");
+        System.out.println("        add and subtract the number computed in Step 2");
+        System.out.println("        inside the parentheses with the x^2 and x terms.");
         System.out.println();
-        System.out.println( a + "(x^2 + " + (b/a) + "x + " + num + " - " + num + ") + " + c );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        System.out.println( a + "(x^2 + " + (b/a) + "*x + " + num + " - " + num + ") + " + c );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
-        
-        
+
         // Step 4
         System.out.println();
         System.out.println();
-        System.out.println("Step 4: Move the term that we subtracted in Step 3 to the");
-        System.out.println("outside of the parentheses by distributing the factored");
-        System.out.println("number from Step 1.");
+        System.out.println("Step 4: Move the term that we subtracted in step 3 to the");
+        System.out.println("        outside of the parentheses by distributing the");
+        System.out.println("        factored number from step 1.");
         System.out.println();
-        System.out.println( a + "(x^2 + " + (b/a) + "x + " + num + ") - " + (a*num) + " + " + c );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        System.out.println( a + "(x^2 + " + (b/a) + "*x + " + num + ") - " + (a*num) + " + " + c );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
-        
-        
+
         // Step 5
         System.out.println();
         System.out.println();
         System.out.println("Step 5: Factor what is left in the parentheses into a ");
-        System.out.println("perfect square.  Use the work in step 2 to help figure out");
-        System.out.println("what the factors will be.  Also, combine constants on the ");
-        System.out.println("outside of the parentheses.");
+        System.out.println("        perfect square.  Use the work in step 2 to help");
+        System.out.println("        figure out what the factors will be.");
         System.out.println();
-        System.out.println( a + "(x + " + (b/(2*a)) + ")^2 + " + (c-a*num) );
-        
-        System.out.println();
-        System.out.print("Type in C to continue: ");
+        System.out.println( a + "(x + " + (b/(2*a)) + ")^2 + " + (c - a*num) );
+
+        System.out.print("Type in C and hit enter to continue: ");
         answer = kb.next();
         
+        // Fractional answers:
         
+        System.out.println();
+        System.out.println( a + "(x + " + convertDecimalToFraction(b/(2*a)) + ")^2 + " + convertDecimalToFraction(c - a*num) );
+
+        System.out.print("Type in C and hit enter to continue: ");
+        answer = kb.next();
     }
+
+    public static String convertDecimalToFraction(double x)
+    {
+        if (x < 0)
+        {
+            return "-" + convertDecimalToFraction(-x);
+        }
+        
+        double tolerance = 1.0E-13;
+        double h1=1;
+        double h2=0;
+        double k1=0;
+        double k2=1;
+        double b = x;
+        
+        do
+        {
+            double a = Math.floor(b);
+            double aux = h1;
+            h1 = a*h1+h2;
+            h2 = aux;
+            aux = k1;
+            k1 = a*k1+k2;
+            k2 = aux;
+            b = 1/(b-a);
+        } while (Math.abs(x-h1/k1) > x*tolerance);
+
+        return (int)h1+"/"+(int)k1;
+    }
+
 }
